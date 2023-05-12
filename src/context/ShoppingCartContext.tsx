ea3f69c22e148600c0c922107f2daafddf86ecfa
TommyNguyen <timsnguyen@hotmail.com>
2023-05-12 00:17:39 -0700

    add context

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, ReactNode } from "react";
+
+const ShoppingCartContext = createContext({});
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+    const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    function getItemQuantity(id: number) {
+        return cartItems.find(item => item.id === id)?.quantity || 0
+    }
+  
+    
+    return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,20 @@
+import { Row, Col } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export function Store() {
     return (
-        <div>
+        <>
         <h1>Store</h1>
-        </div>
+
+        {/* xs={1} - Extra Small Screen will create 1 column */}
+        {/* md={2} - Medium Screen will create 2 columns */}
+        {/* lg={3} - Large Screen will create 3 columns */}
+        <Row xs={1} md={2} lg={3} className="g-3">
+            {storeItems.map((item) => (
+                <Col key={item.id}><StoreItem {...item} /></Col>
+            ))}
+        </Row>
+        </>
     )
 }
